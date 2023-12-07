3b4e52ad3a2a47ef67ecf556bcc1a50da9d78222
Sarequl Basar <sarequl@proton.me>
2023-12-07 17:37:56 +0600

    add

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,16 +5,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB23XvF97rTgdxdK9ZTD69KgnYevss2ml4",
-  authDomain: "samiras-kitchen-c359b.firebaseapp.com",
-  projectId: "samiras-kitchen-c359b",
-  storageBucket: "samiras-kitchen-c359b.appspot.com",
-  messagingSenderId: "644371839253",
-  appId: "1:644371839253:web:2e628b04b99caf1025511c"
+  apiKey: "AIzaSyDQD0fOt8n3r39UIGBGbXNbu-AVX5exXug",
+  authDomain: "samiras-kitchen-89d6e.firebaseapp.com",
+  projectId: "samiras-kitchen-89d6e",
+  storageBucket: "samiras-kitchen-89d6e.appspot.com",
+  messagingSenderId: "811635806495",
+  appId: "1:811635806495:web:8677525eb4196e2692e02a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
 
+export default app;
 
